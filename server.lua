abae202471b825deeb3433c19c356e2c99c43ed7
Turbo <79483260+InfiniteTurbo@users.noreply.github.com>
2023-04-16 13:18:16 -0400

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,8 +1,4 @@
 RegisterServerEvent('fireworks')
 AddEventHandler('fireworks', function()
-    if IsPlayerAceAllowed(source, "bz.fwcmds") then
         TriggerClientEvent('frobski-fireworks:start', source)
-    else
-        TriggerClientEvent('bz-noperms', source)
-    end
 end)
